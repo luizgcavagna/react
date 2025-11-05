e7c36949e385f37edd0b7cd2e584802a60eec754
Luiz G Cavagna <luizcavagna@gmail.com>
2025-11-05 16:58:48 -0300

    components

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,9 +1,52 @@
+import Text from "./components/text"
+import TrashIcon from "./assets/icons/trash.svg?react"
+import CheckIcon from "./assets/icons/check.svg?react"
+import PencilIcon from "./assets/icons/pencil.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import XIcon from "./assets/icons/x.svg?react"
+import Icon from "./components/icon"
+import Badge from "./components/badge"
+import Button from "./components/button"
+
 export default function App() {
 
   return (
-    <>
-      <div className="text-pink-dark">Hello World</div>
-    </>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Hello World!
+        </Text>
+        <Text className="text-green-base">
+          Hello World!
+        </Text>
+        <Text variant="body-md-bold">
+          Hello World!
+        </Text>
+        <Text>
+          Hello World!
+        </Text>
+      </div>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>
+          Nova tarefa
+        </Button>
+      </div>
+    </div>
   )
 }
 
