8937871e08fb1ac237b4e2ebc43ef1090ce02ef2
Luiz G Cavagna <luizcavagna@gmail.com>
2025-11-17 16:57:01 -0300

    components

--- todo/src/components/button-icon.tsx
+++ todo/src/components/button-icon.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Icon from "./icon";
 import { cva, type VariantProps } from "class-variance-authority";
+import Skeleton from "./skeleton";
 
 export const buttonIconVariants = cva("inline-flex items-center justify-center cursor-pointer transition group", {
   variants: {
     variant: {
+      none: "",
       primary: "bg-green-base hover:bg-green-dark",
-      secondary: "bg-pink-base hover:bg-pink-dark",
+      secondary: "bg-gray-200 hover:bg-pink-base",
       tertiary: "bg-transparent hover:bg-gray-200"
     },
     size: {
@@ -26,6 +28,7 @@ export const buttonIconVariants = cva("inline-flex items-center justify-center c
 export const buttonIconIconVariants = cva("transition", {
   variants: {
     variant: {
+      none: "",
       primary: "fill-white",
       secondary: "fill-pink-base group-hover:fill-white",
       tertiary: "fill-gray-300 group-hover:fill-gray-400"
@@ -41,7 +44,8 @@ export const buttonIconIconVariants = cva("transition", {
 })
 
 interface ButtonIconProps extends Omit<React.ComponentProps<"button">, "size" | "disabled">, VariantProps<typeof buttonIconVariants>  {
-  icon: React.ComponentProps<typeof Icon>["svg"]
+  icon: React.ComponentProps<typeof Icon>["svg"];
+  loading?: boolean;
 }
 
 export default function ButtonIcon({
@@ -49,9 +53,20 @@ export default function ButtonIcon({
   size,
   disabled,
   className,
+  loading,
   icon,
   ...props
 }: ButtonIconProps) {
+
+  if (loading) {
+    return (
+      <Skeleton
+        rounded="sm"
+        className={buttonIconVariants({ variant: "none", size, className })}
+      />
+    );
+  }
+
   return (
     <button
       className={buttonIconVariants({
